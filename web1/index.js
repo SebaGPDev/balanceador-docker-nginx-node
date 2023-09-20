0e6e8376f89026e07aed3fd983ed5b4b93a06d70
SebaGPDev <sebakpo120@gmail.com>
2023-09-20 14:46:51 -0300

    completed

---
+++ web1/index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Respuesta desde web1\n');
+});
+
+server.listen(80);
